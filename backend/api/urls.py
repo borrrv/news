8b6d2c911d259a74e15559186ea577e953c7fc87
Vladislav Boroday <vrboroday@yandex.ru>
2023-08-13 12:50:53 +0300

    Add comments full requests

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from .views import GetAuthToken
 from rest_framework.routers import DefaultRouter
-from .views import NewsViewSet
+from .views import NewsViewSet, CommentViewSet
 
 
 router = DefaultRouter()
 
 router.register("news", NewsViewSet)
-
+router.register("comment", CommentViewSet, basename="comment")
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/api/views.py
+++ backend/api/views.py
@@ -7,7 +7,9 @@ from .permissions import IsAuthorOrAdmin
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_201_CREATED, HTTP_405_METHOD_NOT_ALLOWED
+from rest_framework.permissions import SAFE_METHODS
+from rest_framework.pagination import PageNumberPagination
 
 
 class GetAuthToken(ObtainAuthToken):
@@ -17,7 +19,7 @@ class GetAuthToken(ObtainAuthToken):
 class NewsViewSet(ModelViewSet):
     queryset = News.objects.all().select_related('author')
     serializer_class = NewsSerializer
-    methods = ['POST', 'PATCH', 'PUT', 'DELETE']
+    methods = ['POST', 'PUT', 'DELETE']
 
     def get_permissions(self):
         if self.request.method in self.methods:
@@ -27,18 +29,45 @@ class NewsViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    @action(methods=['POST'], detail=True)
+    @action(methods=['POST', 'GET'], detail=True)
     def comment(self, request, pk=None):
         news = get_object_or_404(News, pk=pk)
-        user = self.request.user
-        text = request.data['text']
-        comment = Comments.objects.create(
-            author_id=user.id,
-            news=news,
-            text=text,
-        )
-        serializer = CommentSerializer(
-            comment,
-            context={'request': request},
-        )
-        return Response(serializer.data, HTTP_201_CREATED)
+        if request.method == 'POST':
+            user = self.request.user
+            text = request.data['text']
+            comment = Comments.objects.create(
+                author_id=user.id,
+                news=news,
+                text=text,
+            )
+            serializer = CommentSerializer(
+                comment,
+                context={'request': request},
+            )
+            return Response(serializer.data, HTTP_201_CREATED)
+        if request.method == 'GET':
+            comments = Comments.objects.filter(news=news)
+            paginator = PageNumberPagination()
+            result_page = paginator.paginate_queryset(comments, request)
+            serializer = CommentSerializer(
+                result_page,
+                many=True,
+                context={'request': request},
+            )
+            return paginator.get_paginated_response(serializer.data)
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    queryset = Comments.objects.all().select_related('author')
+
+    def get_permissions(self):
+        if self.request.method not in SAFE_METHODS:
+            self.permission_classes = [IsAuthorOrAdmin]
+        return super().get_permissions()
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=HTTP_405_METHOD_NOT_ALLOWED)
+
+    def create(self, request, *args, **kwargs):
+        return Response(status=HTTP_405_METHOD_NOT_ALLOWED)
